67e9e942f75e87c212039b38ad6cc3270922122c
Johongir10 <shavkatovj73@gmail.com>
2024-08-09 08:55:02 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,33 @@
+// const ismBody = document.getElementById("ism");
+// const sonH1 = document.getElementById("son");
+
+// ismBody.style = "background:yellow; display:flex; align-items:center; justify-content:center; height:100vh;"
+
+// // ismBody.style = "background-color:yellow";
+
+// let arr = ['Jahongir', "Sherzod", 12,19,92,02,8,9,209,282,28,29,82]
+
+// let randomResult = arr[(Math.floor(Math.random() * arr.length))]
+
+// sonH1.innerHTML = randomResult
+
+// const Body = document.getElementsByTagName("Body")[0];
+
+// const a=document.createElement("h1")
+// const p=document.createElement("p")
+// a.textContent="promt"
+// p.textContent="jxfdodvofvlkdmfvnsdfvndoifvniofdsvnsidvnsidvidvnisdvnsvksdmvklsdmvskldmvskldvmsdvmsldvmsdl;vmsdkvmsl;dvmlsdvmsdk;lvmsd;lvmsdlvmskldvmksldvmskdvmsdvmsdvmsdmvsdvmsdvmodsvmfsvsfnbjsbflsdvnaldvnadlkvndskvnsjvnsdlvsdvnsuiniwoevniosdvniosvsv"
+// const b=document.createElement("img")
+// b
+// Body.appendChild(a)
+// Body.appendChild(p)
+// Body.appendChild(b)
+
+const name = prompt("Ismingizni kiriting: ");
+
+const title = document.createElement("h1");
+title.textContent = name;
+const body = document.body; 
+body.appendChild(title);
+body.style="background-color:blue;";
+body.style="color:black;background-color:blue; diplay:flex; justify-content:center;align-items:center;height:100vh;";
